4a1ba0bbab8be080be7ec3a204af12d479ab4250
Achille Roussel <achille@segment.com>
2016-06-29 15:57:41 -0700

    fix cloudwatchlogs implementation

---
+++ lib/cloudwatchlogs/client.go
@@ -0,0 +1,159 @@
+package cloudwatchlogs
+
+import (
+	"errors"
+	"strings"
+	"sync"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	"github.com/segmentio/ecs-logs/lib"
+)
+
+type client struct {
+	cmtx   sync.Mutex
+	client *cloudwatchlogs.CloudWatchLogs
+
+	wmtx    sync.Mutex
+	writers map[string]*writer
+}
+
+func newClient() *client {
+	return &client{
+		writers: make(map[string]*writer, 100),
+	}
+}
+
+func (c *client) Open(group string, stream string) (w ecslogs.Writer, err error) {
+	var client *cloudwatchlogs.CloudWatchLogs
+	var token string
+	var writer = c.get(group, stream)
+
+	w = writer
+
+	writer.mutex.Lock()
+	defer writer.mutex.Unlock()
+
+	if len(writer.token) != 0 {
+		// The writer already has a token, this means the log group and streams
+		// have been created for that writer already.
+		return
+	}
+
+	if client, err = c.getAwsClient(); err != nil {
+		return
+	}
+
+	if token, err = createGroupAndStream(client, group, stream); err != nil {
+		// Creating the log group or stream failed, this writer cannot be used.
+		delete(c.writers, joinGroupStream(group, stream))
+		return
+	}
+
+	writer.token = token
+	return
+}
+
+func (c *client) get(group string, stream string) (w *writer) {
+	key := joinGroupStream(group, stream)
+	c.wmtx.Lock()
+
+	if w = c.writers[key]; w == nil {
+		w = &writer{
+			group:  group,
+			stream: stream,
+			parent: c,
+		}
+		c.writers[key] = w
+	}
+
+	c.wmtx.Unlock()
+	return
+}
+
+func (c *client) remove(group string, stream string) {
+	key := joinGroupStream(group, stream)
+	c.wmtx.Lock()
+	delete(c.writers, key)
+	c.wmtx.Unlock()
+}
+
+func (c *client) getAwsClient() (client *cloudwatchlogs.CloudWatchLogs, err error) {
+	c.cmtx.Lock()
+	defer c.cmtx.Unlock()
+
+	if client = c.client; client == nil {
+		if client, err = openAwsClient(); err != nil {
+			return
+		}
+		c.client = client
+	}
+
+	return
+}
+
+func openAwsClient() (client *cloudwatchlogs.CloudWatchLogs, err error) {
+	var region string
+
+	if region, err = getAwsRegion(); err != nil {
+		return
+	}
+
+	client = cloudwatchlogs.New(session.New(&aws.Config{
+		Region: aws.String(region),
+	}))
+	return
+}
+
+func createGroupAndStream(client *cloudwatchlogs.CloudWatchLogs, group string, stream string) (token string, err error) {
+	var result *cloudwatchlogs.DescribeLogStreamsOutput
+
+	// Ignore failures on group and stream creation, describing the stream will
+	// fail later if the group doesn't exist. That way the group creation is
+	// idempotent.
+	client.CreateLogGroup(&cloudwatchlogs.CreateLogGroupInput{
+		LogGroupName: aws.String(group),
+	})
+	client.CreateLogStream(&cloudwatchlogs.CreateLogStreamInput{
+		LogGroupName:  aws.String(group),
+		LogStreamName: aws.String(stream),
+	})
+
+	if result, err = client.DescribeLogStreams(&cloudwatchlogs.DescribeLogStreamsInput{
+		Limit:               aws.Int64(1),
+		LogGroupName:        aws.String(group),
+		LogStreamNamePrefix: aws.String(stream),
+	}); err != nil {
+		// The AWS Go SDK doesn't export error types, this is the best hack I
+		// cloud find to check for this specific error type.
+		//
+		// The documentation says that we can only make 5 calls per second to
+		// this endpoint, but we need the sequence token in order to send events
+		// to streams that already exist.
+		//
+		// If we fail to fetch the stream description we still move on without
+		// a token and let the retry logic around PutLogEvents attempt to handle
+		// the issue.
+		if strings.HasPrefix(err.Error(), "ThrottlingException:") {
+			err = nil
+		}
+		return
+	}
+
+	if len(result.LogStreams) == 0 {
+		err = errDescribeLogStream
+		return
+	}
+
+	token = aws.StringValue(result.LogStreams[0].UploadSequenceToken)
+	return
+}
+
+func joinGroupStream(group string, stream string) string {
+	return group + "::" + stream
+}
+
+var (
+	errDescribeLogStream = errors.New("getting the log stream description failed")
+)
--- lib/cloudwatchlogs/writer.go
+++ lib/cloudwatchlogs/writer.go
@@ -1,124 +1,131 @@
 package cloudwatchlogs
 
 import (
+	"errors"
 	"fmt"
+	"strings"
+	"sync"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	"github.com/segmentio/ecs-logs/lib"
 )
 
-func NewWriter(group string, stream string) (w ecslogs.Writer, err error) {
-	var region string
-
-	if region, err = getAwsRegion(); err != nil {
-		return
-	}
-
-	w = writer{
-		group:  group,
-		stream: stream,
-		client: cloudwatchlogs.New(session.New(&aws.Config{
-			Region: aws.String(region),
-		})),
-	}
-	return
-}
-
 type writer struct {
+	mutex  sync.Mutex
 	group  string
 	stream string
-	client *cloudwatchlogs.CloudWatchLogs
+	token  string
+	parent *client
 }
 
-func (w writer) Close() error {
+func (w *writer) Close() error {
 	return nil
 }
 
-func (w writer) WriteMessage(msg ecslogs.Message) error {
+func (w *writer) WriteMessage(msg ecslogs.Message) error {
 	return w.WriteMessageBatch([]ecslogs.Message{msg})
 }
 
-func (w writer) WriteMessageBatch(batch []ecslogs.Message) (err error) {
-	var stream *cloudwatchlogs.LogStream
-
-	w.ensureCreateGroup()
-	w.ensureCreateStream()
-
-	if stream, err = w.fetchStream(); err != nil {
+func (w *writer) WriteMessageBatch(batch []ecslogs.Message) (err error) {
+	if len(batch) == 0 {
 		return
 	}
 
-	return w.writeBatch(stream, batch)
-}
+	var token *string
+	var result *cloudwatchlogs.PutLogEventsOutput
+	var events = make([]*cloudwatchlogs.InputLogEvent, len(batch))
 
-func (w writer) ensureCreateGroup() {
-	w.client.CreateLogGroup(&cloudwatchlogs.CreateLogGroupInput{
-		LogGroupName: aws.String(w.group),
-	})
-	return
-}
-
-func (w writer) ensureCreateStream() {
-	w.client.CreateLogStream(&cloudwatchlogs.CreateLogStreamInput{
-		LogGroupName:  aws.String(w.group),
-		LogStreamName: aws.String(w.stream),
-	})
-	return
-}
-
-func (w writer) fetchStream() (stream *cloudwatchlogs.LogStream, err error) {
-	var streams *cloudwatchlogs.DescribeLogStreamsOutput
-
-	if streams, err = w.client.DescribeLogStreams(&cloudwatchlogs.DescribeLogStreamsInput{
-		LogGroupName:        aws.String(w.group),
-		LogStreamNamePrefix: aws.String(w.stream),
-		Limit:               aws.Int64(1),
-	}); err != nil {
-		return
-	}
-
-	if len(streams.LogStreams) == 0 {
-		err = fmt.Errorf("failed to fetch log stream info from cloud watch logs (%s: stream not found)", w.stream)
-		return
-	}
-
-	stream = streams.LogStreams[0]
-	return
-}
-
-func (w writer) writeBatch(stream *cloudwatchlogs.LogStream, batch []ecslogs.Message) (err error) {
-	var events []*cloudwatchlogs.InputLogEvent
-
-	if events = makeLogEvents(batch); len(events) == 0 {
-		return
-	}
-
-	_, err = w.client.PutLogEvents(&cloudwatchlogs.PutLogEventsInput{
-		LogEvents:     events,
-		LogGroupName:  aws.String(w.group),
-		LogStreamName: aws.String(w.stream),
-		SequenceToken: stream.UploadSequenceToken,
-	})
-	return
-}
-
-func makeLogEvents(batch []ecslogs.Message) (events []*cloudwatchlogs.InputLogEvent) {
-	events = make([]*cloudwatchlogs.InputLogEvent, 0, len(batch))
-
-	for _, msg := range batch {
+	for i, msg := range batch {
 		// Set the message properties to their zero-value so they are omitted when
 		// serialized to JSON by the String method.
 		ts := msg.Time
 		msg.Group = ""
 		msg.Stream = ""
 		msg.Time = 0
-		events = append(events, &cloudwatchlogs.InputLogEvent{
+		events[i] = &cloudwatchlogs.InputLogEvent{
 			Message:   aws.String(msg.String()),
 			Timestamp: aws.Int64(ts.Milliseconds()),
-		})
+		}
 	}
 
+	// Because of the logic imposed by the AWS API we can only submit one upload
+	// request per log stream at a time due to the sequence token being unique
+	// and usable only once.
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+
+	if w.parent == nil {
+		// Another goroutine has invalidated this writer, giving up.
+		err = errInvalidWriter
+		return
+	}
+
+	if len(w.token) != 0 {
+		token = aws.String(w.token)
+	}
+
+	for attempt := 1; true; attempt++ {
+		if result, err = w.parent.client.PutLogEvents(&cloudwatchlogs.PutLogEventsInput{
+			LogEvents:     events,
+			LogGroupName:  aws.String(w.group),
+			LogStreamName: aws.String(w.stream),
+			SequenceToken: token,
+		}); err == nil {
+			break
+		}
+
+		// The AWS Go SDK doesn't expose the error type but does return the
+		// token in the error message so we attempt to extract it from there
+		// and let the retry logic resubmit the event batch.
+		//
+		// See: https://forums.aws.amazon.com/message.jspa?messageID=676912
+		if token = parseInvalidSequenceTokenException(err); attempt < 3 && token != nil {
+			err = nil
+			continue
+		}
+
+		// The documentation says we have to provide the sequence token when
+		// uploading events to CloudWatchLogs, if an error is returned here
+		// it's likely the token we have is either invalid or something worse
+		// happened.
+		// We remove the writer from it's parent client so a new writer will
+		// be created.
+		w.parent.remove(w.group, w.stream)
+		w.parent = nil
+		return
+	}
+
+	w.token = aws.StringValue(result.NextSequenceToken)
 	return
 }
+
+func parseInvalidSequenceTokenException(err error) (token *string) {
+	msg := err.Error()
+	fmt.Println("<<<", msg)
+
+	if !strings.HasPrefix(msg, "InvalidSequenceTokenException:") {
+		fmt.Println("no prefix")
+		return
+	}
+
+	if lines := strings.Split(msg, "\n"); len(lines) != 0 {
+		msg = lines[0]
+	}
+
+	parts := strings.Split(msg, ":")
+
+	if len(parts) < 3 {
+		fmt.Println("bad parts count:", len(parts))
+		return
+	}
+
+	s := strings.TrimSpace(parts[2])
+	fmt.Println(">>>", s)
+	token = &s
+	return
+}
+
+var (
+	errInvalidWriter = errors.New("the writer was invalidated by another goroutine")
+)
