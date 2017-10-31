6bb6d390bb43f67577e81b415832cbe7b40b3829
Jeremy Jackins <jeremy@segment.com>
2017-10-31 13:23:35 -0700

    add failing test

--- lib/syslog/writer_test.go
+++ lib/syslog/writer_test.go
@@ -1,8 +1,11 @@
 package syslog
 
 import (
+	"crypto/tls"
 	"fmt"
 	"math/rand"
+	"net/url"
+	"os"
 	"testing"
 	"time"
 
@@ -56,5 +59,48 @@ func TestWriter(t *testing.T) {
 		if err := <-errc; err != nil {
 			t.Error(err)
 		}
+	}
+}
+
+func TestGetPool(t *testing.T) {
+	s := os.Getenv("SYSLOG_URL")
+	u, err := url.Parse(s)
+	if err != nil {
+		t.Fatal("SYSLOG_URL must be set")
+	}
+
+	network := u.Scheme
+	address := u.Host
+
+	opts := dialOpts{
+		network: network,
+		address: address,
+		tls: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+		socksProxy: "",
+	}
+	pool1, err := getPool(opts)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer pool1.Close()
+
+	// make a new dialOpts identical to the old one
+	opts = dialOpts{
+		network: network,
+		address: address,
+		tls: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+		socksProxy: "",
+	}
+	pool2, err := getPool(opts)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if pool1 != pool2 {
+		t.Error("pools did not match")
 	}
 }
